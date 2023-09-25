1e6a411563cae9f4532d4bc64c28113b75e7aa95
LeeKoldun <leadkoldun@gmail.com>
2023-09-25 23:27:48 +0300

    Updates

--- OzonParser/Program.cs
+++ OzonParser/Program.cs
@@ -14,46 +14,47 @@ namespace OzonParser {
                 LogSeverity = LogSeverity.Disable
             });
 
-            string address = "https://www.ozon.ru/category/avtomobilnye-kompressory-8577/?category_was_predicted=true&deny_category_prediction=true&from_global=true&text=%D0%BD%D0%B0%D1%81%D0%BE%D1%81+%D0%B0%D0%B2%D1%82%D0%BE%D0%BC%D0%BE%D0%B1%D0%B8%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9";
+            string address = "https://www.ozon.ru/category/smartfony-15502/?category_was_predicted=true&deny_category_prediction=true&from_global=true&text=%D1%81%D0%BC%D0%B0%D1%80%D1%82%D1%84%D0%BE%D0%BD";
+            //string address = "https://www.ozon.ru/category/avtomobilnye-kompressory-8577/?category_was_predicted=true&deny_category_prediction=true&from_global=true&text=%D0%BD%D0%B0%D1%81%D0%BE%D1%81+%D0%B0%D0%B2%D1%82%D0%BE%D0%BC%D0%BE%D0%B1%D0%B8%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9";
 
             using (var browser = new ChromiumWebBrowser(address)) {
                 var initResult = await browser.WaitForInitialLoadAsync();
             
                 if (!initResult.Success) {
-                    Console.WriteLine("Не удалось загрузить сайт!");
+                    Console.WriteLine("Couldn't load site!");
                     return;
                 }
-            
-                await Task.Delay(500);
+
+                Console.WriteLine("Loading site...");
+                await Task.Delay(5000);
 
             
                 var doc = await GetHtmlSource(await browser.GetSourceAsync());
                 var item = doc.QuerySelector(".widget-search-result-container")!;
-                var a = item.FirstElementChild;
+                var itemsWrapper = item.FirstElementChild;
 
                 //foreach ( var child in a )
                 //{
                 //    Console.WriteLine(child.InnerHtml + "\n");
                 //}
 
-                string className = a.FirstElementChild!.ClassName!;
-                className = className.Replace(" ", ".");
+                string className = itemsWrapper.FirstElementChild!.ClassName!.Replace(" ", ".").Insert(0, ".");
+                Console.WriteLine($"Got class name: {className}");
+                var products = doc.QuerySelectorAll(className);
 
-                var products = doc.QuerySelectorAll($".{className}");
-
-                foreach ( var product in products )
-                {
+                foreach( var product in products ) {
                     var refHtml = product.FirstElementChild!.FirstElementChild!;
                     await Console.Out.WriteLineAsync(refHtml.GetAttribute("href"));
                     await Console.Out.WriteLineAsync("\n");
                 }
                 Console.WriteLine("\n\n\n");
+                await Console.Out.WriteLineAsync($"Got {products.Length} item on current page");
 
                 browser.Dispose();
             }
             
             Console.WriteLine("Done!");
-            Console.WriteLine("Нажмите любую клавишу, чтобы закрыть консоль");
+            Console.WriteLine("PRESS ANY BUTTON TO EXIT CONSOLE");
 
             Console.ReadKey();
         }
