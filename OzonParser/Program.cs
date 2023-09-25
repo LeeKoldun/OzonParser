5dc31a84c8d819553c3f4692683439c6e75b25be
LeeKoldun <leadkoldun@gmail.com>
2023-09-25 19:37:55 +0300

    Getting prods

--- OzonParser/Program.cs
+++ OzonParser/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Drawing;
-using AngleSharp;
-using AngleSharp.Dom;
+﻿using AngleSharp.Dom;
 using AngleSharp.Html.Dom;
 using AngleSharp.Html.Parser;
 using CefSharp;
@@ -11,44 +9,58 @@ namespace OzonParser {
     internal static class Program {
         
         private static async Task Main() {
-            await Cef.InitializeAsync(new CefSettings() {
+            await Cef.InitializeAsync(new CefSettings()
+            {
                 LogSeverity = LogSeverity.Disable
             });
 
-            string address = "https://www.ozon.ru/product/1195594338";
+            string address = "https://www.ozon.ru/category/avtomobilnye-kompressory-8577/?category_was_predicted=true&deny_category_prediction=true&from_global=true&text=%D0%BD%D0%B0%D1%81%D0%BE%D1%81+%D0%B0%D0%B2%D1%82%D0%BE%D0%BC%D0%BE%D0%B1%D0%B8%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9";
+
+            using (var browser = new ChromiumWebBrowser(address)) {
+                var initResult = await browser.WaitForInitialLoadAsync();
             
-            using var browser = new ChromiumWebBrowser(address);
-            var initResult = await browser.WaitForInitialLoadAsync();
+                if (!initResult.Success) {
+                    Console.WriteLine("Не удалось загрузить сайт!");
+                    return;
+                }
             
-            if (!initResult.Success) {
-                Console.WriteLine("Не удалось загрузить сайт!");
-                return;
-            }
-            
-            await Task.Delay(500);
+                await Task.Delay(500);
 
             
-            var doc = await GetHtmlSource(await browser.GetSourceAsync());
-            var items = doc.QuerySelectorAll(".i8t.ti8");
+                var doc = await GetHtmlSource(await browser.GetSourceAsync());
+                var item = doc.QuerySelector(".widget-search-result-container")!;
+                var a = item.FirstElementChild;
 
-            foreach (var item in items) {
-                Console.WriteLine(item.Html());
+                //foreach ( var child in a )
+                //{
+                //    Console.WriteLine(child.InnerHtml + "\n");
+                //}
+
+                string className = a.FirstElementChild!.ClassName!;
+                className = className.Replace(" ", ".");
+
+                var products = doc.QuerySelectorAll($".{className}");
+
+                foreach ( var product in products )
+                {
+                    var refHtml = product.FirstElementChild!.FirstElementChild!;
+                    await Console.Out.WriteLineAsync(refHtml.GetAttribute("href"));
+                    await Console.Out.WriteLineAsync("\n");
+                }
+                Console.WriteLine("\n\n\n");
+
+                browser.Dispose();
             }
             
             Console.WriteLine("Done!");
             Console.WriteLine("Нажмите любую клавишу, чтобы закрыть консоль");
+
             Console.ReadKey();
-            
-            Cef.Shutdown();
-            browser.Dispose();
         }
 
         private static async Task<IHtmlDocument> GetHtmlSource(string html) {
             var parser = new HtmlParser();
             return await parser.ParseDocumentAsync(html);
-            // var config = Configuration.Default;
-            // var context = BrowsingContext.New(config);
-            // return await context.OpenAsync(req => req.Content(html));
         }
         
     }
